3240b7f1908e661364e19a3c46e9b7ee688dcc65
Ruul1999 <dominguezraul1999@gmail.com>
2025-06-04 14:33:46 -0600

    feat: configuration of ProductoController for search products done and implemented services

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using gcomercial_api.Context;
+using gcomercial_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,10 @@ builder.Services.AddDbContext<CuernavacaDbContext>(options =>
 
 builder.Services.AddDbContext<GestionComercialDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GestionComercialConnection")));
+
+
+builder.Services.AddScoped<IAlmacenService, AlmacenService>();
+builder.Services.AddScoped<IProductoService, ProductoService>();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Services/Producto/IProductoService.cs
@@ -0,0 +1,7 @@
+﻿namespace gcomercial_api.Services
+{
+    public interface IProductoService
+    {
+        Task<object> BuscarProductosAsync(int page, int pageSize, string search, string filters);
+    }
+}
